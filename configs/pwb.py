a7b7665898fe13dcf8a075bc532352df5d6d30e3
Khurram Javed <kjaved@ualberta.ca>
2020-07-13 22:20:42 -0600

    Update pwb.py

--- configs/pwb.py
+++ configs/pwb.py
@@ -13,6 +13,6 @@ class PwbParameters(configargparse.ArgParser):
         self.add('--rank', type=int, help='Rank to specify hyper-parameter when if multiple values are passed for the same parameter', default=0)
         self.add('--env1', type=float, help='Value of latent variable in one part of the seen MDP', default=0.01)
         self.add('--env2', type=float, help='Value of the latent variable in the other part of the seen MDP', default=0.24)
-        self.add('--name', help='Name of experiment', default="colored_mnist/")
+        self.add('--name', help='Name of experiment', default="PwB/")
         self.add('--output-dir', help='Directory where results and trained model is stored', default="../results/")
         self.add('--l1-penalty', type=float, nargs='+', help='L1 reguarlization', default=[1e-3])
